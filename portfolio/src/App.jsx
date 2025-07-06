c0ff76b18825ccf2806ec9d98628080ca8aeb470
Geeth Seneviratne <it23226128@my.sliit.lk>
2025-07-06 17:12:14 +0530

    Add about page
    Create an about page using reactjs and css

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Home from './components/Home/Home'
 import Navbar from './components/Navbar/Navbar'
+import About from './components/About/About'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Home/>
+       <About/>
       
 
 
