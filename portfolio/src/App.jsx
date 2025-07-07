2004a915179c5aab6fda2c02c61625f45c079ebd
Geeth Seneviratne <it23226128@my.sliit.lk>
2025-07-07 17:14:17 +0530

    Update App.jsx file
    Update App.jsx file

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -3,6 +3,7 @@ import Home from './components/Home/Home'
 import Navbar from './components/Navbar/Navbar'
 import About from './components/About/About'
 import Service from './components/Service/Service'
+import MyWork from './components/MyWork/MyWork'
 
 const App = () => {
   return (
@@ -11,7 +12,7 @@ const App = () => {
       <Home/>
        <About/>
        <Service/>
-      
+      <MyWork/>
 
 
     </div>
